58f85401720581a125456c315bfb9291f3646aa9
Tsemharb <gorbachevart@gmail.com>
2019-08-31 17:24:36 +0300

    deleted unnecessary if __name__ == "__main__"

--- SickTick/__init__.py
+++ SickTick/__init__.py
@@ -3,8 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello Flask!!!</h1>'
-
-if __name__ == '__main__':
-    app.run()
-
+    return '<h1>Hello, Flask!!!</h1>'
