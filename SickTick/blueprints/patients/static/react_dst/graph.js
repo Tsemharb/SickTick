6c4fd157256821610043a3f4ae1d8864501fbdfa
Tsemharb <gorbachevart@gmail.com>
2019-10-05 12:17:32 +0300

    doc parser in separate notebook

--- SickTick/blueprints/patients/static/react_dst/graph.js
+++ SickTick/blueprints/patients/static/react_dst/graph.js
@@ -6,7 +6,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-import draw from './draw_graph/draw.js';
+import draw from './graph_components/draw.js';
 
 var Graph = function (_React$Component) {
 	_inherits(Graph, _React$Component);
@@ -36,12 +36,5 @@ var Graph = function (_React$Component) {
 
 	return Graph;
 }(React.Component);
-
-// function Graph(s){
-// 	console.log(s.patients);
-// 	return(
-// 		<div>{s.length}</div>
-// 	)
-// }
 
 export default Graph;
